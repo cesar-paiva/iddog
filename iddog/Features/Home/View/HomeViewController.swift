e92a939043c3f5023077c93d3b0ace3430e76cff
cesar paiva <cesar.giupponi@gmail.com>
2019-12-13 00:15:49 -0300

    Adicionada home view controller e implementada navegação após login

---
+++ iddog/Features/Home/View/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  iddog
+//
+//  Created by Cesar Paiva on 12/12/19.
+//  Copyright © 2019 Cesar Paiva. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+    }
+
+}
+
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
